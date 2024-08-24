7091d8c648aece75dda4839c4b8a02e8bb3240e6
ETYonamine <etyonamine@users.noreply.github.com>
2024-08-24 13:36:44 -0300

    docker compose

--- source/front/vite-project/vite.config.js
+++ source/front/vite-project/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {              
+    strictPort: true,
+    port: 5173,
+  } ,
+  server:{
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://localhost:5173"
+  }
 })
